6ba24b3270d028ac4c97b18baac8c0b18e0e5a98
hwa <HyunhwaShin@gmail.com>
2021-04-20 15:25:02 +0900

    Room DB start _ Contact.kt

---
+++ app/src/main/java/com/example/refrigerator_kotlin/ROOM/Contact.kt
@@ -0,0 +1,17 @@
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName="contact")
+data class Contact(
+    @PrimaryKey(autoGenerate = true) var id : Long?,
+    @ColumnInfo(name ="foodName") var foodName : String,
+    @ColumnInfo(name = "limitDate") var limitDate : String,
+    @ColumnInfo(name = "upDown") var upDown : String,
+    @ColumnInfo(name = "path") var path : String,
+    @ColumnInfo(name = "memo") var memo : String
+
+
+) {
+    constructor() : this(null,"","","","","")
+}
