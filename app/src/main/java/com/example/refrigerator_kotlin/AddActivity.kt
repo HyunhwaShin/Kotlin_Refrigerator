426327b26c5f5bbd5e85a31e726433feeb09fff7
hwa <HyunhwaShin@gmail.com>
2021-04-28 15:39:06 +0900

    switch button revise2

--- app/src/main/java/com/example/refrigerator_kotlin/AddActivity.kt
+++ app/src/main/java/com/example/refrigerator_kotlin/AddActivity.kt
@@ -6,6 +6,7 @@ import android.app.DatePickerDialog
 import android.os.Bundle
 import android.widget.CompoundButton
 import android.widget.DatePicker
+import android.widget.Switch
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
@@ -27,30 +28,33 @@ class AddActivity : AppCompatActivity(){
         if (intent != null && intent.hasExtra(EXTRA_FOOD_NAME) && intent.hasExtra((EXTRA_FOOD_LIMITDATE))
             && intent.hasExtra((EXTRA_FOOD_ID))){
             add_foodName.setText(intent.getStringExtra(EXTRA_FOOD_NAME))
-            add_limitDate.setText(intent.getStringExtra(EXTRA_FOOD_LIMITDATE))
+            add_limitDate.text = intent.getStringExtra(EXTRA_FOOD_LIMITDATE)
             id = intent.getLongExtra(EXTRA_FOOD_ID, -1)
         }
         //저장 button click
         add_save_Button.setOnClickListener {
             val foodName = add_foodName.text.toString().trim()
             val limitDate = add_limitDate.text.toString()
-            val upDown = add_upDown.isSelected
             val memo = add_memo.text.toString()
 
-            add_upDown.setOnCheckedChangeListener(object : CompoundButton.OnCheckedChangeListener{
+            //switch
+            val switch = findViewById<Switch>(R.id.add_upDown)
+
+            switch.setOnCheckedChangeListener(object : CompoundButton.OnCheckedChangeListener{
                 override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
                     //off -> on(냉장)
-                    if(isChecked){
-                            state = "냉장"
+                    state = if(isChecked){
+                        "냉장"
                     }else {
-                        state = "냉동"
+                        "냉동"
                     }
                 }
             })
+            val upDown = state.toString()
 
            // DatePickerDialog.OnDateSetListener 로 limitDate 설정하기
 
-            //필수사항인 (foodName & limitDate) check
+            //필수사항인 (foodName & limitDate) check 후 저장
             if(foodName.isEmpty() || limitDate.isEmpty()){
                 Toast.makeText(this, "음식명 과 유통기한을 입력해주세요.", Toast.LENGTH_LONG).show()
             }else{
